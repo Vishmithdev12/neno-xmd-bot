e6300e2c983f904b4e459649d0b7c0c262630815
Vishmithdev12 <156363377+Vishmithdev12@users.noreply.github.com>
2025-08-28 23:37:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your sesson id with out mulvin~",
-  OWNER_NUM: process.env.OWNER_NUM || "94726228243",
+  SESSION_ID: process.env.SESSION_ID || "OeIWnJQT#Xnm0ybeEpxnw04mBIJF6TCaNFmJorWPQCOUb4-1Ya1s",
+  OWNER_NUM: process.env.OWNER_NUM || "94712293208",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
